54edd970f352d60c09601be1fcfc1bd8e8da421c
sd <myexample@blah.com>
2015-12-14 02:03:17 +0100

    wokring passing json string

--- workspace/web5/src/com/example/web5/Diagram.java
+++ workspace/web5/src/com/example/web5/Diagram.java
@@ -1,7 +1,5 @@
 package com.example.web5;
 
-import java.util.List;
-
 import com.vaadin.annotations.JavaScript;
 import com.vaadin.annotations.StyleSheet;
 import com.vaadin.ui.AbstractJavaScriptComponent;
@@ -11,12 +9,13 @@ import com.vaadin.ui.AbstractJavaScriptComponent;
 @JavaScript({ "d3.min.js", "diagram_connector.js", "jquery-1.11.3.min.js" })
 public class Diagram extends AbstractJavaScriptComponent {
 
-	public void setCoords(final List<Integer> coords) {
-		getState().setCoords(coords);
-	}
-
 	@Override
 	public DiagramState getState() {
 		return (DiagramState) super.getState();
 	}
+
+	public void addTreeData(String data) {
+		getState().treeData = data;
+	}
+
 }
--- workspace/web5/src/com/example/web5/Web5UI.java
+++ workspace/web5/src/com/example/web5/Web5UI.java
@@ -1,12 +1,18 @@
 package com.example.web5;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+
 import javax.servlet.annotation.WebServlet;
+
+import org.json.simple.JSONObject;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Component;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -15,18 +21,47 @@ import com.vaadin.ui.VerticalLayout;
 public class Web5UI extends UI {
 
 	final VerticalLayout layout = new VerticalLayout();
-	final Diagram diagram = new Diagram();
+	String s = "{\"name\": \"flare\",\"size\": 3812}";
 
 	@WebServlet(value = "/*", asyncSupported = true)
-	@VaadinServletConfiguration(productionMode = false, ui = Web5UI.class)
+	@VaadinServletConfiguration(productionMode = false, ui = Web5UI.class/*
+																			 * ,
+																			 * widgetset
+																			 * =
+																			 * "com.example.web5.widgetset"
+																			 */)
 	public static class Servlet extends VaadinServlet {
 	}
 
 	@Override
 	protected void init(VaadinRequest request) {
 
+		JSONObject obj = new JSONObject();
+
+		LinkedHashMap m1 = new LinkedHashMap();
+		LinkedHashMap childrenObj = new LinkedHashMap();
+		childrenObj.put("name", "BetweennessCentrality");
+		childrenObj.put("size", new Integer(3534));
+		LinkedList l1 = new LinkedList();
+		l1.add(childrenObj);
+		obj.put("name", "foo");
+		obj.put("children", l1);
+		// m1.put("children", l1);
+		StringWriter out = new StringWriter();
+		try {
+			obj.writeJSONString(out);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String jsonText = out.toString();
+
+		Diagram diagram = new Diagram();
+		diagram.addTreeData(jsonText);
+		diagram.setHeight("600px");
+		diagram.setWidth("800px");
 		layout.setSpacing(true);
-		layout.addComponent((Component) diagram);
+		layout.addComponent(diagram);
 		setContent(layout);
 	}
 
