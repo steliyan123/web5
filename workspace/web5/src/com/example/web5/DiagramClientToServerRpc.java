7fa6297313c358d0b80b79260313f523f8650231
sd <myexample@blah.com>
2016-01-17 14:20:08 +0100

    simplified rpc js See: https://vaadin.com/wiki/-/wiki/Main/Simplified%20RPC%20using%20JavaScript

--- workspace/web5/src/com/example/web5/DiagramClientToServerRpc.java
+++
@@ -1,8 +0,0 @@
-package com.example.web5;
-
-import com.vaadin.shared.communication.ServerRpc;
-
-public interface DiagramClientToServerRpc extends ServerRpc {
-	// TODO
-	public void onPlotClick();
-}
